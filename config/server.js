a5ca38d393ce7a0672c02e6a399b2b4acaf83faf
kaveh <kaveh>
2021-05-22 18:16:50 -0400

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a8e346ea24d471d5d6e04e9ffb206fe9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b795741c-bf04-462d-a88b-8c572a5fbddb'
+};
